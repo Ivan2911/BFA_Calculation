a6ab510ab1f925b7cc70e7fea53efada49731560
Ivandros Awom <norvin2911@gmail.com>
2022-09-14 23:39:18 -0500

    base working app

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('bfaCalc.html')
+
+@app.route("/handle_calculation", methods=['POST'])
+def output_calc():
+    return render_template('outputCalc.html')
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+    
